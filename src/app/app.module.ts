30b3aa29da783372bd00cb10e02788e74543faad
Rishikakar <67696019+Rishikakar@users.noreply.github.com>
2020-07-06 20:52:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShowproductlistComponent } from './showproductlist/showproductlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProductlistComponent,
+    NavBarComponent,
+    ShowproductlistComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/showproductlist/showproductlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showproductlist',
+  templateUrl: './showproductlist.component.html',
+  styleUrls: ['./showproductlist.component.css']
+})
+export class ShowproductlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
