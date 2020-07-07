b521db82a4c7e6782e1c81b4eba4f3b9d3b4207e
Rishikakar <67696019+Rishikakar@users.noreply.github.com>
2020-07-07 18:28:22 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
@@ -8,21 +8,21 @@ import {ShowproductlistComponent} from './showproductlist/showproductlist.compon
 
 const routes: Routes = [
   {
-  path : "login",
-  component : LoginComponent
+    path: "login",
+    component: LoginComponent
   },
   {
-    path : "register",
-  component : RegisterComponent
+    path: "register",
+    component: RegisterComponent
   },
   {
-    path : "productlist",
-  component : ProductlistComponent
+    path: "productlist",
+    component: ProductlistComponent
   },
   {
-    path : "showproductlist",
-    component : ShowproductlistComponent
-  }
+    path: "showproductlist",
+    component: ShowproductlistComponent
+  },
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProductlistComponent } from './productlist/productlist.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShowproductlistComponent } from './showproductlist/showproductlist.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
     LoginComponent,
     RegisterComponent,
     ProductlistComponent,
-    NavBarComponent,
-    ShowproductlistComponent
+    ShowproductlistComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
